3a9be3b34bebbc0e7f1b43979be5627ad43fb32a
blar khalid <blarharki@gmail.com>
2018-09-04 08:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var  driver={};
 function updateDriverWithKeyAndValue (driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
   
 function deleteFromDriverByKey (driver, key) {
   const newObj = Object.assign({}, driver);
